87b59928c5c8fdce3181539bcb5c5ff365c8fbfd
juandajuwan <you@example.com>
2021-08-07 10:14:04 +0700

    first commit

---
+++ EVALUASI.3.no.3.js
@@ -0,0 +1,5 @@
+var nilaiSantri = [32,33,31,45,94,74,67,51,98,39,87,78,92,61,66,39,54,46,15,55,38,87,78];
+let lolos = nilaiSantri.filter (a => a <= 50)
+let lolos2 = nilaiSantri.filter (b => b <= 20)
+console.log(lolos);
+console.log(lolos2);
